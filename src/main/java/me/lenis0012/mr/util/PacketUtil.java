fc21d31666f3f7c578e7960907de4451ee4e2a46
Lennart ten Wolde <0p1q9o2w@hotmail.nl>
2013-07-02 13:00:52 +0200

    Updated to 1.6.1 R0.1

--- src/main/java/me/lenis0012/mr/util/PacketUtil.java
+++ src/main/java/me/lenis0012/mr/util/PacketUtil.java
@@ -4,16 +4,16 @@ import java.lang.reflect.Field;
 import java.util.Random;
 import java.util.logging.Level;
 
-import net.minecraft.server.v1_5_R3.DataWatcher;
-import net.minecraft.server.v1_5_R3.EntityPlayer;
-import net.minecraft.server.v1_5_R3.MathHelper;
-import net.minecraft.server.v1_5_R3.Packet24MobSpawn;
-import net.minecraft.server.v1_5_R3.Packet29DestroyEntity;
-import net.minecraft.server.v1_5_R3.Packet38EntityStatus;
+import net.minecraft.server.v1_6_R1.DataWatcher;
+import net.minecraft.server.v1_6_R1.EntityPlayer;
+import net.minecraft.server.v1_6_R1.MathHelper;
+import net.minecraft.server.v1_6_R1.Packet24MobSpawn;
+import net.minecraft.server.v1_6_R1.Packet29DestroyEntity;
+import net.minecraft.server.v1_6_R1.Packet38EntityStatus;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_5_R3.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_6_R1.entity.CraftPlayer;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
