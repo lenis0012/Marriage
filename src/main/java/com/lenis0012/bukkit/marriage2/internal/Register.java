1b7ab6c5510c376c33077d6b522bf9a65c98b64c
Lennart ten Wolde <0p1q9o2w@hotmail.nl>
2014-11-13 17:53:47 +0100

    Fix @Register interface

--- src/main/java/com/lenis0012/bukkit/marriage2/internal/Register.java
+++ src/main/java/com/lenis0012/bukkit/marriage2/internal/Register.java
@@ -1,5 +1,12 @@
 package com.lenis0012.bukkit.marriage2.internal;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Register {
 	String name();
 	
