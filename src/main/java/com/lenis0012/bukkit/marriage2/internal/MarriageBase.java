928b07d14f10b0dec0a9759860fd58c5b2a6ae29
Lennart ten Wolde <lenny.devmail@gmail.com>
2023-05-15 16:12:23 +0200

    fix: remove unused classpath scanner

--- src/main/java/com/lenis0012/bukkit/marriage2/internal/MarriageBase.java
+++ src/main/java/com/lenis0012/bukkit/marriage2/internal/MarriageBase.java
@@ -1,7 +1,5 @@
 package com.lenis0012.bukkit.marriage2.internal;
 
-import com.google.common.collect.Lists;
-import com.google.common.reflect.ClassPath;
 import com.lenis0012.bukkit.marriage2.Marriage;
 import com.lenis0012.bukkit.marriage2.commands.Command;
 import com.lenis0012.bukkit.marriage2.misc.BConfig;
@@ -9,23 +7,14 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public abstract class MarriageBase implements Marriage {
     protected final MarriagePlugin plugin;
-    private final ClassPath classPath;
     private MarriageCommandExecutor commandExecutor;
 
     public MarriageBase(MarriagePlugin plugin) {
         this.plugin = plugin;
-        try {
-            this.classPath = ClassPath.from(getClass().getClassLoader());
-        } catch(IOException e) {
-            throw new RuntimeException("Failed to intialize class path!", e);
-        }
     }
 
     void enable() {
@@ -69,35 +58,5 @@ public abstract class MarriageBase implements Marriage {
 
     public MarriageCommandExecutor getCommandExecutor() {
         return commandExecutor;
-    }
-
-    @SuppressWarnings("unchecked")
-    protected <T> List<Class<? extends T>> findClasses(String pkg, Class<T> type, Object... params) {
-        List<Class<? extends T>> list = Lists.newArrayList();
-        for(ClassPath.ClassInfo info : classPath.getTopLevelClassesRecursive(pkg)) {
-            try {
-                Class<?> clazz = Class.forName(info.getName());
-                if(type.isAssignableFrom(clazz) && !type.equals(clazz)) {
-                    list.add((Class<? extends T>) clazz);
-                }
-            } catch(Exception e) {
-                plugin.getLogger().log(Level.WARNING, "Failed to intiate class", e);
-            }
-        }
-
-        return list;
-    }
-
-    protected <T> List<T> findObjects(String pkg, Class<T> type, Object... params) {
-        List<T> list = Lists.newArrayList();
-        for(Class<? extends T> clazz : findClasses(pkg, type)) {
-            try {
-                list.add(type.cast(clazz.getConstructors()[0].newInstance(params)));
-            } catch(Exception e) {
-                plugin.getLogger().log(Level.WARNING, "Failed to construct class", e);
-            }
-        }
-
-        return list;
     }
 }
